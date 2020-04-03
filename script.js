62ccfb2e10398aaaeb89b838095fa8f2d0f8dd7c
appsius <ilyas.ustunkaya234434@gmail.com>
2020-04-03 17:00:52 +0300

    Play Again

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const msgEl = document.getElementById('msg');
-
 const randomNum = getRandomNumber();
-
-console.log('Number:', randomNum);
 
 window.SpeechRecognition =
 	window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -20,6 +17,45 @@ function onSpeak(e) {
 	checkNumber(msg);
 }
 
+// Write what user speaks
+function writeMessage(msg) {
+	msgEl.innerHTML = `
+      <div>You said: </div>
+      <span class="box">${msg}</span>
+   `;
+}
+
+// Check msg against number
+function checkNumber(msg) {
+	const num = +msg;
+
+	// Check if valid number
+	if (Number.isNaN(num)) {
+		msgEl.innerHTML += '<div>That is not a valid number</div>';
+		return;
+	}
+
+	// Check in range
+	if (num > 100 || num < 1) {
+		msgEl.innerHTML += '<div>Number must be between 1 and 100</div>';
+		return;
+	}
+
+	// Check number
+	if (num === randomNum) {
+		document.body.innerHTML = `
+         <h2>Congrats! You won! <br/><br/>
+            It was ${num}
+         </h2>
+         <button id="play-again" class="play-again">Play Again</button>
+      `;
+	} else if (num > randomNum) {
+		msgEl.innerHTML += '<div>GO LOWER</div>';
+	} else {
+		msgEl.innerHTML += '<div>GO HIGHER</div>';
+	}
+}
+
 // Generate random number
 function getRandomNumber() {
 	return Math.floor(Math.random() * 100) + 1;
@@ -27,3 +63,13 @@ function getRandomNumber() {
 
 // Speak result
 recognition.addEventListener('result', onSpeak);
+
+// End SR service
+recognition.addEventListener('end', () => recognition.start());
+
+// Play again
+document.body.addEventListener('click', e => {
+	if (e.target.id === 'play-again') {
+		window.location.reload();
+	}
+});
